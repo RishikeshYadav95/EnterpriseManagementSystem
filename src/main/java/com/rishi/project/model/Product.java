912590407aeb95087a48015e81f79afcad1fe450
RishikeshYadav95 <rishikesh21095@gmail.com>
2023-08-20 08:34:04 -0400

    Added product model java class

---
+++ src/main/java/com/rishi/project/model/Product.java
@@ -0,0 +1,71 @@
+package com.rishi.project.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "product")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "category")
+	private String category;
+	
+	@Column(name = "price")
+	private String price;
+	
+	public Product() {
+		
+	}
+
+	public Product(long id, String name, String category, String price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.category = category;
+		this.price = price;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+}
