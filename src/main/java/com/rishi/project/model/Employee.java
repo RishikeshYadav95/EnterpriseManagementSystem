5cfd5ee35d3fdf9fd7e7360afe42c70935a1cf7b
Rishikesh Yadav <rishikesh21095@gmail.com>
2023-08-27 05:54:34 -0400

    Update Employee.java

--- src/main/java/com/rishi/project/model/Employee.java
+++ src/main/java/com/rishi/project/model/Employee.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+//This class is the model for Employee.
 @Entity
 @Table(name= "employee")
 public class Employee {
